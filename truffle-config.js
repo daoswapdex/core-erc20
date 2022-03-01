01301dac2d2a279ad22aeef8e783f92a731680d5
daoswap002 <daoswap@daoswap.cc>
2022-03-01 17:04:25 +0800

    add bsc network config

--- truffle-config.js
+++ truffle-config.js
@@ -5,7 +5,7 @@ const mnemonic = fs.readFileSync(".mnemonic").toString().trim();
 
 module.exports = {
   networks: {
-    testnet: {
+    hecotestnet: {
       provider: () => new HDWalletProvider(mnemonic, 'https://http-testnet.hecochain.com'),
       network_id: 256,
       // from: '0xa9bB710996d6ed61B83a5EAB583bAe683199c2de',        // Account to send txs from (default: accounts[0])
@@ -15,7 +15,7 @@ module.exports = {
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
     },
-    mainnet: {
+    hecomainnet: {
       provider: () => new HDWalletProvider(mnemonic, 'https://http-mainnet.hecochain.com'),
       network_id: 128,
       // from: '0xa9bB710996d6ed61B83a5EAB583bAe683199c2de',        // Account to send txs from (default: accounts[0])
@@ -24,6 +24,20 @@ module.exports = {
       // confirmations: 2,    // # of confs to wait between deployments. (default: 0)
       // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+    },
+    bsctestnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://data-seed-prebsc-1-s1.binance.org:8545`),
+      network_id: 97,
+      // confirmations: 10,
+      // timeoutBlocks: 200,
+      // skipDryRun: true
+    },
+    bscmainnet: {
+      provider: () => new HDWalletProvider(mnemonic, `https://bsc-dataseed1.binance.org`),
+      network_id: 56,
+      // confirmations: 10,
+      // timeoutBlocks: 200,
+      // skipDryRun: true
     }
   },
 
